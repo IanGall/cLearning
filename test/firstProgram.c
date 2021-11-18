c577064336ae4806780c721eaae522f428cdae05
钱怡凡 <2213947010@qq.com>
2021-11-18 16:32:05 +0800

    Making Your Programs More Powerful with #include and #define

---
+++ test/firstProgram.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+  /* code */
+  printf("Just one small step for coders. One giant leap for");
+  printf(" programmers!\n");
+  char c = 'a';
+  float a = .85;
+  printf("I am %.2f percent ready to move on ", a);
+
+  printf("I am learning the %c programming language\n", 'C');
+  printf("I have just completed Chapter %d\n", 2);
+  printf("I am %.1f percent ready to move on ", 99.9);
+  printf("to the next chapter!\n");
+
+  printf("Write code\n");
+  printf("Learn c");
+  return 0;
+}
