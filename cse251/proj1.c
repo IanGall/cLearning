5efb6a559802aebf2e33617dbd490e0d1bc46fbf
钱怡凡 <2213947010@qq.com>
2021-12-24 10:44:00 +0800

    Project 1 - Integral of sinc function

--- cse251/proj1.c
+++ cse251/proj1.c
@@ -5,19 +5,35 @@ double fx(double x);
 
 double delta(int n, int a, int b);
 
+double compute(int n, double a, double b);
+
 int main() {
-    int x;
-    int n = 1883;
+    int n = 10;
+    double a = 0.1;
+    double b = 0.2;
+    double pre = compute(n, a, b);
+    printf("%d %.9lf\n", n, pre);
+    for (n = 11; n <= 100000; n++) {
+        double res = compute(n, a, b);
+        double decrease = pre - res;
+        printf("%d %.9lf %e\n", n, res, decrease);
+        pre = res;
+        if (decrease < 1e-10) {
+            break;
+        }
+    }
+}
+
+double compute(int n, double a, double b) {
     int i;
-    int a = -1;
-    int b = 1;
     double d;
     double I = 0;
     d = delta(n, a, b);
     for (i = 1; i < n + 1; ++i) {
-        I += fx(a + (i - 0.5) * d) * d;
+        double decrease = fx(a + (i - 0.5) * d) * d;
+        I += decrease;
     }
-    printf("%.9lf", I);
+    return I;
 }
 
 double delta(int n, int a, int b) {
@@ -26,6 +42,9 @@ double delta(int n, int a, int b) {
 }
 
 double fx(double x) {
+    if (x == 0) {
+        return 1;
+    }
     double d = sin(M_PI * x);
     return d / (M_PI * x);
 }
