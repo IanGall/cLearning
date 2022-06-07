48b120fce576f2b818a865176a2e137f974dc9ce
IanQian <qianyifan1996@gmail.com>
2022-06-07 09:42:37 +0800

    Practice Problem 2.27 (solution page 188) overflow test

---
+++ csapp/chapter2/2.27-OverflowTest.c
@@ -0,0 +1,18 @@
+//
+// Created by IanQian on 2022/6/7.
+//
+#include <stdio.h>
+#include <limits.h>
+
+int uadd_ok(unsigned x, unsigned y);
+
+int main() {
+    unsigned int y = 1;
+    unsigned int x = UINT_MAX - y;
+    printf("uadd_ok(%u,%u):%d\n", x, y, uadd_ok(x, y));
+    printf("uadd_ok(%u,%u):%d\n", UINT_MAX, y, uadd_ok(UINT_MAX, y));
+}
+
+int uadd_ok(unsigned int x, unsigned int y) {
+    return x + y > x;
+}
