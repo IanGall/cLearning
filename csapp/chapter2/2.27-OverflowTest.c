097fdbe6ec2a0f3579e5b327b59008e6b77ba3c9
IanQian <qianyifan1996@gmail.com>
2022-06-07 10:02:26 +0800

    squash! Practice Problem 2.27 (solution page 188) overflow test

--- csapp/chapter2/2.27-OverflowTest.c
+++ csapp/chapter2/2.27-OverflowTest.c
@@ -9,10 +9,12 @@ int uadd_ok(unsigned x, unsigned y);
 int main() {
     unsigned int y = 1;
     unsigned int x = UINT_MAX - y;
+    unsigned int z = 0;
     printf("uadd_ok(%u,%u):%d\n", x, y, uadd_ok(x, y));
     printf("uadd_ok(%u,%u):%d\n", UINT_MAX, y, uadd_ok(UINT_MAX, y));
+    printf("uadd_ok(%u,%u):%d\n", UINT_MAX, z, uadd_ok(UINT_MAX, z));
 }
 
 int uadd_ok(unsigned int x, unsigned int y) {
-    return x + y > x;
+    return x + y >= x;
 }
